5c84f95b3e129bd2e3c8b6435536142394beeece
Aron Ricardo Perez-Lopez <arpl@mit.edu>
2017-12-02 01:21:17 -0500

    Add welcome text and spinner to MainActivity

--- app/src/main/java/dev/m1hackers/funtimes/MainActivity.java
+++ app/src/main/java/dev/m1hackers/funtimes/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Spinner imgNumSpinner = findViewById(R.id.img_num_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> imgNumSpinnerAdapter = ArrayAdapter.createFromResource(
+                this, R.array.img_num_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        imgNumSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        imgNumSpinner.setAdapter(imgNumSpinnerAdapter);
+
+
     }
 
     @Override
